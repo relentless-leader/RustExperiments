94a0e65fa16c88a8064cdebd15c6a39783f7a60f
Rishabh Bhatia <bhatiarishabh63@gmail.com>
2025-06-30 05:31:03 -0700

    trying out enum.

---
+++ coin_collection/src/main.rs
@@ -0,0 +1,35 @@
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+    // --snip--
+}
+
+#[derive(Debug)]
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {state:?}!");
+            25
+        }
+    }
+}
+
+fn main() {
+    value_in_cents(Coin::Quarter(UsState::Alaska));
+    let x = Coin::Quarter(UsState::Alaska);
+    println!("Coin is {x:?}!");
+    let y = Coin::Penny;
+    let z = value_in_cents(y);
+    println!("Coin is {z:?}!");
+}
