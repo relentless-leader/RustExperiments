b408fc2b86a6b2b757fd468a66e3efc169f8501e
Rishabh Bhatia <bhatiarishabh63@gmail.com>
2025-06-30 06:13:40 -0700

    Pattern matching using if let.

--- coin_collection/src/main.rs
+++ coin_collection/src/main.rs
@@ -1,8 +1,18 @@
-#[derive(Debug)]
+#[derive(Debug)] // so we can inspect the state in a minute
 enum UsState {
     Alabama,
     Alaska,
     // --snip--
+}
+
+impl UsState {
+    fn existed_in(&self, year: u16) -> bool {
+        match self {
+            UsState::Alabama => year >= 1819,
+            UsState::Alaska => year >= 1959,
+            // -- snip --
+        }
+    }
 }
 
 #[derive(Debug)]
@@ -13,23 +23,23 @@ enum Coin {
     Quarter(UsState),
 }
 
-fn value_in_cents(coin: Coin) -> u8 {
-    match coin {
-        Coin::Penny => 1,
-        Coin::Nickel => 5,
-        Coin::Dime => 10,
-        Coin::Quarter(state) => {
-            println!("State quarter from {state:?}!");
-            25
-        }
+fn describe_state_quarter(coin: Coin) -> Option<String> {
+    let state = if let Coin::Quarter(state) = coin {
+        state
+    } else {
+        return None;
+    };
+    println!("State is {state:?}");
+
+    if state.existed_in(1900) {
+        Some(format!("{state:?} is pretty old, for America!"))
+    } else {
+        Some(format!("{state:?} is relatively new."))
     }
 }
 
 fn main() {
-    value_in_cents(Coin::Quarter(UsState::Alaska));
-    let x = Coin::Quarter(UsState::Alaska);
-    println!("Coin is {x:?}!");
-    let y = Coin::Penny;
-    let z = value_in_cents(y);
-    println!("Coin is {z:?}!");
+    if let Some(desc) = describe_state_quarter(Coin::Quarter(UsState::Alaska)) {
+        println!("{desc}");
+    }
 }
