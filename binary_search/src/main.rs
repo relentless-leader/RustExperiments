46763cdb38088ee2fa4f4c1793e948c7322cf49c
Rishabh Bhatia <bhatiarishabh63@gmail.com>
2025-07-24 05:35:23 -0700

    Added binary search.

--- binary_search/src/main.rs
+++ binary_search/src/main.rs
@@ -1,4 +1,18 @@
 fn main() {
-    let sortedItems: Vec<i32> = vec![1,2,3,4,5,6,7,8,9,10];
-    println!("sortedItems: {:?}", sortedItems);
+    let sorted_items: Vec<i32> = vec![1,2,3,4,5,6,7,8,9,10];
+    println!("sortedItems: {:?}", sorted_items);
+    let find_value = 2;
+    let mut left = 0;
+    let mut right = sorted_items.len()-1;
+    loop {
+        let mid = (left + right)/2;
+        if sorted_items[mid] > find_value {
+            right = mid - 1;
+        } else if sorted_items[mid] == find_value {
+            println!("Found the value at {}", mid+1);
+            break;
+        } else {
+            left = mid + 1;
+        }
+    }
 }
