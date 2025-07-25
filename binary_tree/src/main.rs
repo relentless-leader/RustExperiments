b1df1d805f5a58e40d2388f666e44ffcbc12e65e
Rishabh Bhatia <bhatiarishabh63@gmail.com>
2025-07-25 07:04:45 -0700

    Playing with box and reference in rust to get basic struct for binary tree

--- binary_tree/src/main.rs
+++ binary_tree/src/main.rs
@@ -1,14 +1,41 @@
+#[derive(Debug)]
 struct Node {
-    left: Box<Node>,
-    right: Box<Node>,
+    left: Option<Box<Node>>,
+    right: Option<Box<Node>>,
     value: i32,
 }
 
+fn insert_left(current: &mut Node, val: i32) {
+    println!("Hello current: {:p}", &current);
+    let left = Node {
+        left: None,
+        right: None,
+        value: val
+    };
+    current.left = Some(Box::new(left));
+}
+
+fn insert_right(current: &mut Node, val: i32) {
+    println!("Hello current: {:p}", &current);
+    let right = Node {
+        left: None,
+        right: None,
+        value: val
+    };
+    current.right = Some(Box::new(right));
+}
+
 fn main() {
-    let root = Node {
-        left = None,
-        right = None,
-        value = 10
-    }
-    println!("Hello, world!");
+    let mut root = Node {
+        left: None,
+        right: None,
+        value: 10
+    };
+    println!("Hello root ref: {:p}", &root);
+    insert_left(&mut root, 12);
+    println!("Hello root: {:?}", root);
+    println!("Hello root ref: {:p}", &root);
+    println!("Hello root.left: {:?}", root.left);
+    println!("Hello root.left ref: {:p}", &root.left);
+    println!("Hello root.left ref: {:p}", &*root.left.unwrap());
 }
