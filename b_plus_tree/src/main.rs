dca1d1849a37a71e2cd0753a23761218f094b3d6
Rishabh Bhatia <bhatiarishabh63@gmail.com>
2025-07-29 06:54:08 -0700

    Got the basic structure of a B+ tree.

--- b_plus_tree/src/main.rs
+++ b_plus_tree/src/main.rs
@@ -1,3 +1,13 @@
+#[derive(Debug)]
+struct Node {
+    order: i32,
+    values: Vec<String>,
+    keys: Option<Vec<Vec<Box<Node>>>>,
+    next_key: Option<Box<Node>>,
+    parent: Option<Box<Node>>,
+    is_leaf: bool
+}
+
 fn main() {
     println!("Hello, world!");
 }
